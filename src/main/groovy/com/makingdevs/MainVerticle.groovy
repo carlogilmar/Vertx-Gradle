b3cf35ed27dbae5f43e61bd2d1b35d439bb38397
carlogilmar <carlogilmar12@gmail.com>
2017-04-10 10:39:50 -0500

    Deploy de un verticle con gradle

--- src/main/groovy/com/makingdevs/MainVerticle.groovy
+++ src/main/groovy/com/makingdevs/MainVerticle.groovy
@@ -6,6 +6,21 @@ import io.vertx.core.DeploymentOptions
 class MainVerticle extends AbstractVerticle {
   @Override
   void start(){
-    vertx.deployVerticle("src/main/groovy/com/makingdevs/Webserver.groovy")
+    vertx.deployVerticle("src/main/groovy/com/makingdevs/Verticle.groovy"){ res->
+      if(res.succeeded()){
+
+        def text = "4716 9309 9602 0116"
+
+        vertx.eventBus().send("com.carlogilmar.reader", text){ reply ->
+          if(reply.succeeded())
+            println "* Reader Status: ${reply.result().body()}"
+          else
+            println "Sin recibir respuesta"
+        }
+      }
+      else{
+        println "Deploy error"
+      }
+    }
   }
 }
---
+++ src/main/groovy/com/makingdevs/Verticle.groovy
@@ -0,0 +1,73 @@
+vertx.eventBus().consumer("com.carlogilmar.reader"){ message ->
+  def text = message.body()
+  message.reply("[It Works]")
+
+  def instruction = []
+  def counter = 0
+
+  if(instruction.size>5){
+    println "counter mayor a cinco"
+    println counter
+    vertx.eventBus().send("com.carlogilmar.showInstruction", instruction)
+  }
+  text.eachLine{ item ->
+    vertx.eventBus().send("com.carlogilmar.prepareCard", item){replyCardComplete ->
+      if(replyCardComplete.succeeded()){
+        println "---------------"*5
+        println replyCardComplete.result().body().dump()
+        println "---------------"*5
+        instruction.add(replyCardComplete.result().body())
+        println "Tamanio de lista ${instruction.size}"
+      }
+    }
+    counter++
+  }
+
+}
+
+vertx.eventBus().consumer("com.carlogilmar.showInstruction"){message->
+  println "Recibiendo el bonch de tarjetazos"
+  println message.body().dump()
+}
+
+vertx.eventBus().consumer("com.carlogilmar.prepareCard"){ message ->
+  def card = message.body()
+  vertx.eventBus().send("com.carlogilmar.services", card){ reply ->
+    if(reply.succeeded()){
+      message.reply(reply.result().body())
+    }
+    else{ println "No hay respuesta de -services verticle-" }
+  }
+}
+
+vertx.eventBus().consumer("com.carlogilmar.services"){ message ->
+  def creditCard = message.body()
+  def resultCreditCard = [id:creditCard]
+
+  vertx.eventBus().send("com.carlogilmar.ccvsServices", creditCard){ reply->
+    if(reply.succeeded()){
+      resultCreditCard.ccvs=reply.result().body()
+    }
+    else{
+      println "Sin respuesta de ccvsServices"
+    }
+  }
+
+  vertx.eventBus().send("com.carlogilmar.database", creditCard){ reply->
+    if(reply.succeeded()){
+      resultCreditCard.db=reply.result().body()
+      //respuesta del verticle
+      message.reply(resultCreditCard)
+    }
+    else{
+      println "Sin respuesta de database"
+    }
+  }
+}
+
+vertx.eventBus().consumer("com.carlogilmar.ccvsServices"){ message ->
+  message.reply("${message.body()} 123")
+}
+vertx.eventBus().consumer("com.carlogilmar.database"){ message ->
+  message.reply("${message.body()} qwery01")
+}
