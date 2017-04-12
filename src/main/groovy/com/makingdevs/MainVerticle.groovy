32665f21198c4d7c8c2d3eec0cd252623c7088de
carlogilmar <carlogilmar12@gmail.com>
2017-04-12 12:00:16 -0500

    Limpiando la integración con gradle

--- src/main/groovy/com/makingdevs/MainVerticle.groovy
+++ src/main/groovy/com/makingdevs/MainVerticle.groovy
@@ -6,22 +6,6 @@ import io.vertx.core.DeploymentOptions
 class MainVerticle extends AbstractVerticle {
   @Override
   void start(){
-    vertx.deployVerticle("src/main/groovy/com/makingdevs/Verticle.groovy"){ res->
-      if(res.succeeded()){
-        println "Deploy verticle"
-
-        def text = "4716 9309 9602 0116"
-        println "Mandando mensaj"
-        vertx.eventBus().send("com.carlogilmar.reader", text){ reply ->
-          if(reply.succeeded())
-            println "* Reader Status: ${reply.result().body()}"
-          else
-            println "Sin recibir respuesta"
-        }
-      }
-      else{
-        println "Deploy error"
-      }
-    }
+    vertx.deployVerticle("src/main/groovy/com/makingdevs/Webserver.groovy")
   }
 }
