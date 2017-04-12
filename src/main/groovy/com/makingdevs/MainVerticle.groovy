a6a3fc0a11c41952112098c03036a91c5d7abe23
carlogilmar <carlogilmar12@gmail.com>
2017-04-12 12:02:52 -0500

    Agregando comentario para el deploy

--- src/main/groovy/com/makingdevs/MainVerticle.groovy
+++ src/main/groovy/com/makingdevs/MainVerticle.groovy
@@ -9,3 +9,19 @@ class MainVerticle extends AbstractVerticle {
     vertx.deployVerticle("src/main/groovy/com/makingdevs/Webserver.groovy")
   }
 }
+
+
+/*
+* For example:
+* You can deploy a main verticle with this:
+* Remember write the complete path to the verticles
+
+def options = [instances:4]
+
+vertx.deployVerticle("com/makingdevs/BatchCardVerticle.groovy")
+vertx.deployVerticle("com/makingdevs/WsVerticle.groovy")
+vertx.deployVerticle("com/makingdevs/ProcessorVerticle.groovy", options)
+vertx.deployVerticle("com/makingdevs/ClientWsCardVerticle.groovy", options)
+vertx.deployVerticle("com/makingdevs/StatusVerticle.groovy")
+
+*/
